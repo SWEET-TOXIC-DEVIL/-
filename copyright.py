899c8148a0a6bbbd44f145ef9d840a1b47fbabf6
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-05-14 19:47:01 +0530

    copyright.py

--- copyright.py
+++ copyright.py
@@ -49,8 +49,7 @@ async def start(_, msg):
         ],
         [
           InlineKeyboardButton("𝖴𝖯𝖣𝖠𝖳𝖤", url=f"https://t.me/SweetToxicHeroku_CC")
-        ]
-        [
+        ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
